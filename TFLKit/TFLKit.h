9ccc355222384a863f53048f40e99e3f99ea61dc
Jeevan Thandi <jeevan@airlatech.com>
2019-11-16 00:28:06 +0000

    Initial Commit

---
+++ TFLKit/TFLKit.h
@@ -0,0 +1,19 @@
+//
+//  TFLKit.h
+//  TFLKit
+//
+//  Created by Jeevan Thandi on 16/11/2019.
+//  Copyright © 2019 Airla Tech Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TFLKit.
+FOUNDATION_EXPORT double TFLKitVersionNumber;
+
+//! Project version string for TFLKit.
+FOUNDATION_EXPORT const unsigned char TFLKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TFLKit/PublicHeader.h>
+
+
